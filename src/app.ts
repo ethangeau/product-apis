cf030ce096756f24dfe3a67c48c5190737faf095
ethangeau <yebing@student.unimelb.edu.au>
2023-05-24 22:34:22 +1000

    add routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import productRoutes from "./routes/product.route";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+app.use(productRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
