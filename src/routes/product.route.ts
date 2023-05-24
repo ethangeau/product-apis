437f44014e0e7ed8d9eeb75fa4d0d49af19a23c1
ethangeau <yebing@student.unimelb.edu.au>
2023-05-24 22:19:20 +1000

    implement routes

---
+++ src/routes/product.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import * as productController from "../controllers/product.controller";
+
+const router = express.Router();
+
+router.post('/products', productController.createProduct);
+router.put('/products/:id', productController.updateProduct);
+router.get('/products', productController.getProducts);
+router.get('/products', productController.getProduct);
+router.delete('/products/:id', productController.deleteProduct);
+
+export default router;
