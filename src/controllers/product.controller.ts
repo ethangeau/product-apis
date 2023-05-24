1cf7f6e61668bd7ac2662709a5eb35cbc944a4fd
ethangeau <yebing@student.unimelb.edu.au>
2023-05-24 22:04:44 +1000

    implement controllers

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,12 +2,84 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
 
-export const updateProduct = async (req, res) => {};
+  const { name, description, stock, quantity, category, status } = req.body;
+  
+  const product = await prisma.product.create({
+      data: {
+          name,
+          description,
+          stock,
+          quantity,
+          category,
+          status,
+      },
+  });
+  
+  res.json(product);
+};
 
-export const getProducts = async (req, res) => {};
+export const updateProduct = async (req, res) => {
 
-export const getProduct = async (req, res) => {};
+  const { id } = req.params;
+  const { name, description, stock, quantity, category, status } = req.body;
 
-export const deleteProduct = async (req, res) => {};
+  const product = await prisma.product.update({
+      where: { id: Number(id) },
+      data: {
+          name,
+          description,
+          stock,
+          quantity,
+          category,
+          status,
+      },
+  });
+
+  res.json(product);
+};
+
+export const getProducts = async (req, res) => {
+
+  const { name, quantity, stock, category, status} = req.query;
+
+  const products = await prisma.product.findMany({
+      where: {
+          name: name as string,
+          quantity: Number(quantity),
+          stock: Number(stock),
+          category: category as string,
+          status: status === 'true' ? true : false,
+      },
+  });
+
+  res.json(products);
+};
+
+export const getProduct = async (req, res) => {
+
+  const { name, id } = req.query;
+  const condition = name ? { name: name as string } : { id: Number(id) };
+
+  const product = await prisma.product.findFirst({
+    where: condition,
+  });
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  res.json(product);
+};
+
+export const deleteProduct = async (req, res) => {
+
+  const { id } = req.params;
+
+  const product = await prisma.product.delete({
+      where: { id: Number(id) },
+  });
+  
+  res.json(product);
+};
