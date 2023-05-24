aed61c3f623d750b32189e2960e0d4f5665d4347
ethangeau <yebing@student.unimelb.edu.au>
2023-05-24 21:32:13 +1000

    creat backend skeleton

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createProduct = async (req, res) => {};
+
+export const updateProduct = async (req, res) => {};
+
+export const getProducts = async (req, res) => {};
+
+export const getProduct = async (req, res) => {};
+
+export const deleteProduct = async (req, res) => {};
